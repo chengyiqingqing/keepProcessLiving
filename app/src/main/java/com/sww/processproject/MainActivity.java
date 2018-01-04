1faae3e561b4aa039d05285b5b319d892795627e
chengyiqingqing <17839221829@163.com>
2018-01-05 01:43:01 +0800

    finish it

---
+++ app/src/main/java/com/sww/processproject/MainActivity.java
@@ -0,0 +1,25 @@
+﻿package com.sww.processproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //双进程防杀死。
+        startService(new Intent(this,FirstService.class));
+        startService(new Intent(this,SecondService.class));
+        //JobService保活。  它执行了，但是start和stop未必会被调度。
+        startService(new Intent(this,MyJobDaemonService.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
