58d9f631df2deda71cf0b3f3d4401cd9228a97d6
shaowenwen <shaowenwen@amberweather.com>
2018-01-09 12:17:01 +0800

    account sync is worked

--- app/src/main/java/com/sww/processproject/account/AccountAuthenticatorService.java
+++ app/src/main/java/com/sww/processproject/account/AccountAuthenticatorService.java
@@ -3,11 +3,13 @@ package com.sww.processproject.account;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -18,30 +20,37 @@ import android.util.Log;
 public class AccountAuthenticatorService extends Service {
 
     private AccountAuthenticator accountAuthenticator;
-
+    private Handler mHandler=new Handler();
+    private static final String TAG = "sww";
 
     @Override
     public void onCreate() {
         accountAuthenticator = new AccountAuthenticator(this);
         accountAuthenticator.toString();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Log.e("AccountAuthenticator", accountAuthenticator.toString());
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, accountAuthenticator.toString());
+
+            }
+        },5000);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand: " );
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        if (intent.getAction().equals(
-                android.accounts.AccountManager.ACTION_AUTHENTICATOR_INTENT))
+        if (intent.getAction().equals(android.accounts.AccountManager.ACTION_AUTHENTICATOR_INTENT))
             return accountAuthenticator.getIBinder();
+
         return null;
     }
 
-    private static class AccountAuthenticator extends
-            AbstractAccountAuthenticator {
+    private static class AccountAuthenticator extends AbstractAccountAuthenticator {
         final Context context;
 
         public AccountAuthenticator(Context context) {
@@ -55,11 +64,13 @@ public class AccountAuthenticatorService extends Service {
                                  String[] requiredFeatures, Bundle options)
                 throws NetworkErrorException {
 //            Intent intent = new Intent(context, LoginActivity.class);
-//            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE,
-//                    response);
-//            Bundle bundle = new Bundle();
-//            bundle.putParcelable(AccountManager.KEY_INTENT, intent);
-            return null;
+            Intent intent = new Intent("com.sww.processproject.account.SyncService");
+            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE,
+                    response);
+            Bundle bundle = new Bundle();
+            bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+            return bundle;
+//            return null;
         }
 
         @Override
--- app/src/main/java/com/sww/processproject/account/SyncService.java
+++ app/src/main/java/com/sww/processproject/account/SyncService.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SyncResult;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -22,6 +23,7 @@ public class SyncService extends Service {
 
     private static final Object syncLock = new Object();
     private static SyncAdapter syncAdapter = null;
+    private static final String TAG = "sww";
 
     @Override
     public void onCreate() {
@@ -38,22 +40,34 @@ public class SyncService extends Service {
         return syncAdapter.getSyncAdapterBinder();
     }
 
-
+    public static Handler  handler=new Handler();
     class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+
+        public Context context;
 
         public SyncAdapter(Context context, boolean autoInitialize) {
             super(context, autoInitialize);
+            this.context=context;
         }
-
+        //do sync here
         @Override
         public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-//            getContext().getContentResolver().notifyChange(AppAccountProvider.getUri(account.type), null, false);
-            //do sync here
-            Log.v(getClass().getName(),"onPerformSync");
-            Intent intent = new Intent();
+             //kill app ，clear app data ； force stop三者都可以活过来。(已验证：moto)
+            Log.v(TAG,"onPerformSync");
+            //1.验证，它自己起来的。和下面这段代码没有关系。()
+            /*Intent intent = new Intent();
             ComponentName componentName = new ComponentName(account.type, account.name);
             intent.setComponent(componentName);
-            getContext().startService(intent);
+            getContext().startService(intent);*/
+            //2.设置延时强制执行。
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startAccountSync(context);
+                }
+            },10000);
+            //3.在这里设置周期轮询
         }
     }
 
@@ -61,24 +75,76 @@ public class SyncService extends Service {
      * 利用帐号同步机制拉活
      * @param context
      */
-    public static void startAccountSync(Context context){
+    public static void startAccountSync(final Context context){ //好了这样就成了10秒调用一次。
+        Log.e(TAG, "startAccountSync: --- 0" );
         String accountType = "com.sww.processproject";
         AccountManager accountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
         Account account = null;
         Account[] accounts = accountManager.getAccountsByType(accountType);
         if(accounts.length>0){
+            Log.e(TAG, "startAccountSync:  -- 1 " );
             account = accounts[0];
         }else {
-            account = new Account("test", accountType);
+            account = new Account("sww", accountType);
+            Log.e(TAG, "startAccountSync:  -- 2 " );
         }
 
-        if(accountManager.addAccountExplicitly(account,null,null)){
-            String authority = accountType;
-            long sync_interval = 15*60;
-            ContentResolver.setIsSyncable(account,authority,1);
-            ContentResolver.setSyncAutomatically(account, authority, true);  //自动同步
-            ContentResolver.addPeriodicSync(account, authority, new Bundle(), sync_interval);//设置同步时间间隔
+
+
+        final String authority = accountType;
+        if(!accountManager.addAccountExplicitly(account,null,null)){//账号不存在，对该账号进行添加。
+            Log.e(TAG, "startAccountSync:  -- 3" );
+            //如果为true，那么下面的手动同步不生效。
+            ContentResolver.setSyncAutomatically(account, authority, false);  //有网络时是否自动同步
+            ContentResolver.requestSync(account, authority, new Bundle());
+            //手动同步之后，再自动同步。或者延时5s再自动同步。
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    postRunning(context);
+                }
+            },3000);
+           /* //自动同步的设置。
+            long sync_interval = 2;//15*60s
+            ContentResolver.setIsSyncable(account,authority,1);//是否同步；
+            ContentResolver.setSyncAutomatically(account, authority, true);  //有网络时是否自动同步
+            ContentResolver.addPeriodicSync(account, authority, new Bundle(), sync_interval);//设置同步时间间隔*/
+        }else {//账号存在，或者是其他的错误。
+            Log.e(TAG, "startAccountSync:  -- 4" );
+            //手动同步：（如果为true，那么下面的手动同步不生效。）
+            ContentResolver.setSyncAutomatically(account, authority, false);  //有网络时是否自动同步
+            ContentResolver.requestSync(account, authority, new Bundle());
+            //手动同步之后，再自动同步。或者延时5s再自动同步。
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    postRunning(context);
+                }
+            },3000);
+
+
         }
     }
 
+    public static void postRunning(Context context){
+        String accountType = "com.sww.processproject";
+        AccountManager accountManager = (AccountManager)context.getSystemService(Context.ACCOUNT_SERVICE);
+        Account account = null;
+        Account[] accounts = accountManager.getAccountsByType(accountType);
+        if(accounts.length>0){
+            account = accounts[0];
+        }else {
+            account = new Account("sww", accountType);
+        }
+        String authority = accountType;
+        long sync_interval = 2;//15*60s
+        ContentResolver.setIsSyncable(account,authority,1);//是否同步；
+        ContentResolver.setSyncAutomatically(account, authority, true);  //有网络时是否自动同步
+        ContentResolver.addPeriodicSync(account, authority, new Bundle(), sync_interval);//设置同步时间间隔
+    }
+
 }
+
+/**
+ * 账号自动同步和手动同步参考文档。https://www.jianshu.com/p/dc9a2693478e
+ */
